805c93fa5c22ebf92eb602867e82dda9dbfbfdae
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-05-31 17:02:01 +0530

    adding newsapi server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('d166f64f56db4657a9c479c024d7fb12');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+
+const axios = require("axios");
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+    newsapi.v2.everything({
+        q: "mental health",
+        language: 'en',
+        pageSize: 5,
+      }).then(response => {
+          res.json({ result: response.articles });
+      });
+});
+
+app.listen(8080, () => {
+  console.log("server listening on port 8080");
+});
--- src/Components/Pages/BlogPage.tsx
+++ src/Components/Pages/BlogPage.tsx
@@ -1,7 +1,20 @@
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import axios from 'axios'
+
 export const BlogPage = () => {
   const location = useLocation();
   const { state } = location;
   console.log(state);
+
+  const getBlogs = async() => {
+    const res = await axios.get('http://localhost:8080/');
+    console.log(res.data.result);
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, [])
+
   return <>Blog</>;
 };

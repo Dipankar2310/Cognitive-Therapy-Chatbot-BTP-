f909eb1c655f20b627ec4b1535953e8bc2d61eec
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-05-31 18:39:35 +0530

    adding basic blog card

--- src/Components/Pages/BlogPage.tsx
+++ src/Components/Pages/BlogPage.tsx
@@ -1,20 +1,36 @@
-import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { useState } from "react";
 import axios from 'axios'
+import styles from "../../CSSFiles/BlogPage.module.scss";
 
 export const BlogPage = () => {
-  const location = useLocation();
-  const { state } = location;
-  console.log(state);
+  const [blogs, setBlogs] = useState<{ title: string; description: string; }[]>([]);
 
   const getBlogs = async() => {
     const res = await axios.get('http://localhost:8080/');
-    console.log(res.data.result);
+    return res.data.result
   };
 
   useEffect(() => {
-    getBlogs();
+    console.log(blogs);
+  }, [blogs])
+
+  useEffect(() => {
+    getBlogs().then((response) => {
+      setBlogs(response)
+    })
   }, [])
 
-  return <>Blog</>;
+  return(
+    <>
+      <div className="blogs">
+      {blogs.map((blog, index) => (
+          <div key={index} className={styles.blogcard}>
+            <div className={styles.title}>{blog.title}</div>
+            <div>{blog.description}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 };

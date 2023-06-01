3cd195d4a58260a3eabb54aa2f1ef908fc621441
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-06-01 19:01:11 +0530

    searching blogs for userhelptype keyword

--- src/Components/Pages/BlogPage.tsx
+++ src/Components/Pages/BlogPage.tsx
@@ -1,12 +1,30 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useRef } from "react";
 import axios from 'axios'
 import styles from "../../CSSFiles/BlogPage.module.scss";
 import { HiOutlineExternalLink } from 'react-icons/hi';
+import { child, get, getDatabase, ref } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 
 export const BlogPage = (props: any) => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzqB8sbcmuDBo6eMsmjWY84RIAQ_3s6sA",
+    authDomain: "healthgpt-1a9b2.firebaseapp.com",
+    databaseURL: "https://healthgpt-1a9b2-default-rtdb.firebaseio.com",
+    projectId: "healthgpt-1a9b2",
+    storageBucket: "healthgpt-1a9b2.appspot.com",
+    messagingSenderId: "431429000193",
+    appId: "1:431429000193:web:799ed9852f1fbed9eaf400",
+    measurementId: "G-E7Z9SS8BD2"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const auth = getAuth();
+  const dbRef = ref(database);
+  const user = auth.currentUser;
   const [blogs, setBlogs] = useState<{urlToImage:string;  title: string; description: string; author:string; url:string; }[]>([]);
   const [helpTextValue, setHelpTextValue] = useState<string>("mental health");
 
@@ -19,17 +37,31 @@ export const BlogPage = (props: any) => {
     return res.data.result
   };
 
-  useEffect(() => {
-    setHelpTextValue(props.helpText)
-  }, [props.helpText])
+  
+  const getData = async () => {
+    get(child(dbRef, `users/${user?.uid}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const res = snapshot.val();
+          console.log(res.userhelptype)
+          setHelpTextValue(res.userhelptype);
+        }
+      })
+      .catch((error) => {
 
+      });
+  };
 
   useEffect(() => {
-    console.log('the helpText in BlogPage is: ', props.helpText)
-    getBlogs(props.helpText).then((response) => {
+    getData()
+  }, [])
+
+  useEffect(() => {
+    console.log('the helpText in BlogPage is: ', helpTextValue)
+    getBlogs(helpTextValue).then((response) => {
       setBlogs(response)
     })
-  }, [props.helpText])
+  }, [helpTextValue])
 
   return(
     <>
